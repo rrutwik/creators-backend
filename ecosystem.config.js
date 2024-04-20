4a44b382ec3294130c6f8014e5619000fbc07dd3
Rutwik <rutwikpatel08@gmail.com>
2024-04-20 12:09:13 +0530

    Add port 3001

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
       env: { // environment variable
-        PORT: 3000,
+        PORT: 3001,
         NODE_ENV: 'production',
       },
     },
