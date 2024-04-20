032af6e432d6fc7af88a4b315647a0f6582306ec
Rutwik <rutwikpatel08@gmail.com>
2024-04-20 12:47:51 +0530

    Add logs

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -5,7 +5,9 @@ import { NextFunction, Request, Response } from "express";
 export class WebHookController {
   public whatsapp = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log(req.body, req.query, req.params, req.headers);
+      logger.info(`whatsapp webhook called`);
+      const body = req.body;
+      logger.info(`whatsapp webhook body: ${JSON.stringify(body)}`);
       const challenge = req.query["hub.challenge"];
       return res.status(200).send(challenge);
     } catch (error) {
